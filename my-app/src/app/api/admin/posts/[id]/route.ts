e76868bad8d12992ad5f31978f3d87b81f81104b
itakuraharuna <itakuraharuna@dande-lion.co.jp>
2025-07-22 11:44:11 +0900

    🚧 API作成途中

---
+++ my-app/src/app/api/admin/posts/[id]/route.ts
@@ -0,0 +1,170 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  const { id } = params
+
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      include: {
+        postCategories: {
+          include: {
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    return NextResponse.json({ status: 'OK', post: post }, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
+
+// 記事作成のリクエストボディの型
+interface CreatePostRequestBody {
+  title: string
+  content: string
+  categories: { id: number }[]
+  thumbnailUrl: string
+}
+
+// POSTという命名にすることで、POSTリクエストの時にこの関数が呼ばれる
+export const POST = async (request: NextRequest, context: any) => {
+  try {
+    // リクエストのbodyを取得
+    const body = await request.json()
+
+    // bodyの中からtitle, content, categories, thumbnailUrlを取り出す
+    const { title, content, categories, thumbnailUrl }: CreatePostRequestBody = body
+
+    // 投稿をDBに生成
+    const data = await prisma.post.create({
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    })
+
+    // 記事とカテゴリーの中間テーブルのレコードをDBに生成
+    // 本来複数同時生成には、createManyというメソッドがあるが、sqliteではcreateManyが使えないので、for文1つずつ実施
+    for (const category of categories) {
+      await prisma.postCategory.create({
+        data: {
+          categoryId: category.id,
+          postId: data.id,
+        },
+      })
+    }
+
+    // レスポンスを返す
+    return NextResponse.json({
+      status: 'OK',
+      message: '作成しました',
+      id: data.id,
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 })
+    }
+  }
+}
+
+
+// 記事の更新時に送られてくるリクエストのbodyの型
+interface UpdatePostRequestBody {
+  title: string
+  content: string
+  categories: { id: number }[]
+  thumbnailUrl: string
+}
+
+// PUTという命名にすることで、PUTリクエストの時にこの関数が呼ばれる
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }, // ここでリクエストパラメータを受け取る
+) => {
+  // paramsの中にidが入っているので、それを取り出す
+  const { id } = params
+
+  // リクエストのbodyを取得
+  const { title, content, categories, thumbnailUrl }: UpdatePostRequestBody = await request.json()
+
+  try {
+    // idを指定して、Postを更新
+    const post = await prisma.post.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    })
+
+    // 一旦、記事とカテゴリーの中間テーブルのレコードを全て削除
+    await prisma.postCategory.deleteMany({
+      where: {
+        postId: parseInt(id),
+      },
+    })
+
+    // 記事とカテゴリーの中間テーブルのレコードをDBに生成
+    // 本来複数同時生成には、createManyというメソッドがあるが、sqliteではcreateManyが使えないので、for文1つずつ実施
+    for (const category of categories) {
+      await prisma.postCategory.create({
+        data: {
+          postId: post.id,
+          categoryId: category.id,
+        },
+      })
+    }
+
+    // レスポンスを返す
+    return NextResponse.json({ status: 'OK', post: post }, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
+
+
+// DELETEという命名にすることで、DELETEリクエストの時にこの関数が呼ばれる
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }, // ここでリクエストパラメータを受け取る
+) => {
+  // paramsの中にidが入っているので、それを取り出す
+  const { id } = params
+
+  try {
+    // idを指定して、Postを削除
+    await prisma.post.delete({
+      where: {
+        id: parseInt(id),
+      },
+    })
+
+    // レスポンスを返す
+    return NextResponse.json({ status: 'OK' }, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
